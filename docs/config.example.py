aeebff7ac2aac1dc6d6f6c1ef7dd6fdff6372041
volbil <volbilnexus@gmail.com>
2022-01-20 22:13:05 +0200

    Updated config example

--- docs/config.example.py
+++ docs/config.example.py
@@ -2,6 +2,10 @@ secret = "Lorem ipsum dor sit amet"
 bot_key = "TELEGRAM_BOT_KEY"
 chat_id = "CHAT_ID"
 
+captcha = True
+captcha_secret = "SECRET"
+captcha_hostname = "example.com"
+
 params = {
     "host": "0.0.0.0",
     "debug": True,
