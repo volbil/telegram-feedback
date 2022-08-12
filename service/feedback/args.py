093272baa09f8e75cd9129c22a8ec5ac0927db54
obasys <obasystiuk@gmail.com>
2022-08-12 11:44:05 +0300

    Add optional name field

--- service/feedback/args.py
+++ service/feedback/args.py
@@ -2,8 +2,9 @@ from webargs import fields
 
 form_args = {
     "h-captcha-response": fields.Str(missing=None),
+    "name": fields.Email(required=False),
     "email": fields.Email(required=True),
     "message": fields.Str(required=True),
-    "budget": fields.Str(required=True),
+    "budget": fields.Str(required=False),
     "offers": fields.Bool(required=True)
 }
