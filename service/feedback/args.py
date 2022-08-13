89dd1553cc60fb373559cd3910236735075931ac
Oleh Basystiuk <obasystiuk@gmail.com>
2022-08-13 12:28:28 +0300

    Update args.py

--- service/feedback/args.py
+++ service/feedback/args.py
@@ -2,9 +2,9 @@ from webargs import fields
 
 form_args = {
     "h-captcha-response": fields.Str(missing=None),
-    "name": fields.Email(required=False),
+    "name": fields.Email(missing=""),
     "email": fields.Email(required=True),
     "message": fields.Str(required=True),
-    "budget": fields.Str(required=False),
+    "budget": fields.Str(missing=""),
     "offers": fields.Bool(required=True)
 }
